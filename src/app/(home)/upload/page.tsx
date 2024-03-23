6a728a959ef11046ef8e6b227f1cb8a2203c9660
GautamMakadia <gautammakadia3002@gmail.com>
2024-03-24 00:35:20 +0530

    removed un-significant hook - useFormStatus() only works with button/input:submit

--- src/app/(home)/upload/page.tsx
+++ src/app/(home)/upload/page.tsx
@@ -8,7 +8,6 @@ import RecordsView from "@/components/record_table/record_view";
 
 export default function Upload() {
     const [ state , uploadAction ] = useFormState(uploadExcelFile, {msg: "", seating_list: null, error: ""});
-    const { pending } = useFormStatus()
 
     return (
         <>
@@ -41,7 +40,7 @@ export default function Upload() {
                 </form>
                 <div className={styles.uploaded_records}>
                     {
-                        ( pending ? <span>Uploading Records</span> : (state.error) ? <span className="text-red-600">{state.msg}</span> : ((state.seating_list != null) ?
+                        ( (state.error) ? <span className="text-red-600">{state.msg}</span> : ((state.seating_list != null) ?
                             <RecordsView records={state.seating_list} styles={styles}/> : (
                             <div className={styles.desc}> 
                                 <span>Uploaded Records Will Be Displayed Here</span> 
