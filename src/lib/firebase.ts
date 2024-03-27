c99d9a59970ecb07f3d0a15ff8ba7e3073436bb1
GautamMakadia <gautammakadia3002@gmail.com>
2024-03-27 14:24:29 +0530

    done

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,6 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBCQ_auFDOcjALHFRJMlwN-5CGS659jIKA",
-//     authDomain: "gautam-authentication.firebaseapp.com",
-//     projectId: "gautam-authentication",
-//     storageBucket: "gautam-authentication.appspot.com",
-//     messagingSenderId: "38754678314",
-//     appId: "1:38754678314:web:9526fb45cf136209343553",
-//     measurementId: "G-QQMV708NL8"
-// };
-
 const firebaseConfig = {
     apiKey: "AIzaSyAeCifPb6IG_pAxjVb7EAwQ_ti9TZq8I00",
     authDomain: "exam-tracker-au.firebaseapp.com",
